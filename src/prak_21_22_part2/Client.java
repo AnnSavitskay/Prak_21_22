d42b6aa82dca973d80e317f7b3351c439d245b60
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-10-06 16:31:16 +0300

    Prak_21_22_part2

---
+++ src/prak_21_22_part2/Client.java
@@ -0,0 +1,23 @@
+package prak_21_22_part2;
+
+public class Client {
+
+    public Client(){}
+    public String Sit(Chairs chair)
+    {
+        String str = " ";
+        if(chair.type_of_chair() == "MagicChair")
+        {
+            str = "Client sat down on Magic chair";
+        }
+        if(chair.type_of_chair() == "VictorianChair")
+        {
+            str = "Client sat down on Victorian chair";
+        }
+        if(chair.type_of_chair() == "MultifunctionalChair")
+        {
+            str = "Client sat down on Multifunctional chair";
+        }
+        return str;
+    }
+}
